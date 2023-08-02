412a0e518442725ab98297a6037523e11d6ab74f
Jeromeoui <zherong.teo@gmail.com>
2023-08-02 17:52:59 +0800

    Initialized Backend

---
+++ backend/API/Entities/questions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class questions
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string question { get; set; }
+    }
+}
